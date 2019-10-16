df717818e1a5a6b71f77aa61df89f6944206a549
tchirumamilla <56439071+tchirumamilla@users.noreply.github.com>
2019-10-16 18:15:49 +0530

    Add files via upload

---
+++ import.c
@@ -0,0 +1,21 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include"add.h"
+
+static int a=1;
+static int b=2;
+
+static int __init hello_init(void)
+{
+	printk(KERN_ALERT "SUM IS : %d\n",my_add(a,b));
+	return 0;
+}
+
+static int __exit hello_exit(void)
+{
+printk(KERN_ALERT "exit\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
